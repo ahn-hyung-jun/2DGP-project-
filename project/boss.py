04e4a8df8b106d3d7cd26b63a1bd078428a031c1
ahn-hyung-jun(Ative) <43131645+ahn-hyung-jun@users.noreply.github.com>
2018-11-23 00:55:38 +0900

    팔 1번 충돌체크 완료

--- project/boss.py
+++ project/boss.py
@@ -37,12 +37,13 @@ class Boss:
         self.body_state = 0
 
         self.left_arm_HP = 100
-        self.left_arm_x, self.left_arm_y = self.body_x - 60, self.body_y - 40
+        self.left_arm_x, self.left_arm_y = self.body_x - 60, self.body_y - 80
         self.left_arm_image = load_image('boss-arm.png')
         self.left_arm_state = 0
 
         self.right_arm_HP = 100
-        self.right_arm_x, self.right_arm_y = self.body_x + 60, self.body_y - 40
+        self.right_arm_x, self.right_arm_y = self.body_x + 60, self.body_y - 80
+        #self.right_arm_x, self.right_arm_y = 600,500
         self.right_arm_image = load_image('boss-arm.png')
         self.right_arm_state = 0
 
@@ -60,10 +61,31 @@ class Boss:
 
     def right_arm_update(self):
         if self.right_arm_state == 0:
-            pass
+            for hero_bullet in game_world.get_objects(2):
+                if ((hero_bullet.x - self.right_arm_x) ** 2 + (hero_bullet.y - (self.right_arm_y + 60)) ** 2) < (PIXEL_PER_METER * 2) ** 2:
+                    # 라이플은 단순한 데미지
+                    if hero_bullet.state == 1 or hero_bullet.state == 2:
+                        self.right_arm_HP -= hero_bullet.damage
+                    # 바주카는 스플래시 데미지
+                    elif hero_bullet.state == 3:
+                        self.right_arm_HP -= hero_bullet.damage
+                        explosion = Explosion(hero_bullet.x, hero_bullet.y)
+                        game_world.add_object(explosion, 4)
+                    game_world.remove_object(hero_bullet)
 
     def left_arm_update(self):
-        pass
+        if self.right_arm_state == 0:
+            for hero_bullet in game_world.get_objects(2):
+                if ((hero_bullet.x - self.left_arm_x) ** 2 + (hero_bullet.y - (self.left_arm_y + 60)) ** 2) < (PIXEL_PER_METER * 2) ** 2:
+                    # 라이플은 단순한 데미지
+                    if hero_bullet.state == 1 or hero_bullet.state == 2:
+                        self.left_arm_HP -= hero_bullet.damage
+                    # 바주카는 스플래시 데미지
+                    elif hero_bullet.state == 3:
+                        self.left_arm_HP -= hero_bullet.damage
+                        explosion = Explosion(hero_bullet.x, hero_bullet.y)
+                        game_world.add_object(explosion, 4)
+                    game_world.remove_object(hero_bullet)
 
 
     def body_update(self):
@@ -72,10 +94,7 @@ class Boss:
                     ((hero_bullet.x - self.body_x)**2 + (hero_bullet.y - (self.body_y - 100))**2 ) < (PIXEL_PER_METER*2)**2 or \
                 ((hero_bullet.x - self.body_x) ** 2 + (hero_bullet.y - (self.body_y)) ** 2) < (PIXEL_PER_METER * 2) ** 2:
                 #라이플은 단순한 데미지
-                if hero_bullet.state == 1:
-                    self.body_HP -= hero_bullet.damage
-                #샷건은 데미지와 넉백
-                elif hero_bullet.state == 2:
+                if hero_bullet.state == 1 or hero_bullet.state == 2:
                     self.body_HP -= hero_bullet.damage
                 #바주카는 스플래시 데미지
                 elif hero_bullet.state == 3:
@@ -93,13 +112,17 @@ class Boss:
 
     def draw_right_arm(self):
         if self.right_arm_state == 0:
-            self.right_arm_image.clip_composite_draw(0, 535 - 50, 70, 50, 0, '', self.right_arm_x, self.right_arm_y,
-                                                 70 * 2.0, 50 * 2.0)
+            self.right_arm_image.clip_composite_draw(0, 535 - 95, 70, 95, 0, '', self.right_arm_x, self.right_arm_y,
+                                                 70 * 2.0, 95 * 2.0)
+
+        elif self.right_arm_state == 1:
+            self.right_arm_image.clip_composite_draw(70, 535 - 95, 80, 95, 0, '', self.right_arm_x, self.right_arm_y,
+                                                     80 * 2.0, 95 * 2.0)
 
     def draw_left_arm(self):
         if self.left_arm_state == 0:
-            self.left_arm_image.clip_composite_draw(0, 535 - 50, 70, 50, 0, 'h', self.left_arm_x, self.left_arm_y,
-                                                70 * 2.0, 50 * 2.0)
+            self.left_arm_image.clip_composite_draw(0, 535 - 95, 70, 95, 0, 'h', self.left_arm_x, self.left_arm_y,
+                                                70 * 2.0, 95 * 2.0)
 
 
 
